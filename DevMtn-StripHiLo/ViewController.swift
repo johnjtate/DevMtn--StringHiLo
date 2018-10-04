8907a974c1c9eca68f3caae5e5f83c0fd0e953f6
John Tate <johntate@Johns-MacBook-Pro.local>
2018-10-04 17:12:15 -0600

    initial commit

---
+++ DevMtn-StripHiLo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DevMtn-StripHiLo
+//
+//  Created by John Tate on 10/4/18.
+//  Copyright © 2018 John Tate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
