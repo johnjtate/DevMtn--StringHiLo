54da310c319fa49c31b9f2d15da8870defaa50c3
John Tate <johntate@Johns-MacBook-Pro.local>
2018-10-04 17:19:45 -0600

    creates model and model controllers

---
+++ DevMtn-StripHiLo/Model Controller/GamePlayController.swift
@@ -0,0 +1,9 @@
+//
+//  GamePlayController.swift
+//  DevMtn-StripHiLo
+//
+//  Created by John Tate on 10/4/18.
+//  Copyright © 2018 John Tate. All rights reserved.
+//
+
+import Foundation
---
+++ DevMtn-StripHiLo/Model/StickFigure.swift
@@ -0,0 +1,9 @@
+//
+//  StickFigure.swift
+//  DevMtn-StripHiLo
+//
+//  Created by John Tate on 10/4/18.
+//  Copyright © 2018 John Tate. All rights reserved.
+//
+
+import Foundation
