6eed6bbb303a9b185a9001abb15e57f956dd9a44
John Tate <johntate@Johns-MacBook-Pro.local>
2018-10-04 17:55:39 -0600

    adds some functions for gamelay

--- DevMtn-StripHiLo/Model Controller/GamePlayController.swift
+++ DevMtn-StripHiLo/Model Controller/GamePlayController.swift
@@ -18,8 +18,13 @@ class GamePlayController {
     // index 0-4 of stick figure images, with 0 being full clothed and 4 being naked
     var stickFigureImageIndex: Int = 0
     var endGameFlag = false
+    var firstCardSuit: String = ""
+    var firstCardValue: String = ""
+    var secondCardSuit: String = ""
+    var secondCardValue: String = ""
     
     func startGame() {
+        // display some sort of message that the game has started
         round = 1
         playerScore = 0
         endGameFlag = false
@@ -27,14 +32,33 @@ class GamePlayController {
     }
     
     func startRound() {
+        // display some sort of message that the round has started
         round += 1
         playerScore += 1
         // start with index 0 of indices 0-4 as game progresses and player guesses incorrectly
         stickFigureImageIndex = 0
+        
         // draw 2 cards
+        CardController.shared.fetchCard(count: 2) { (cards) in
+            guard let cards = cards else { return }
+            let firstCard = cards[0]
+            let secondCard = cards[1]
+            
+            // set suit and value properties
+            self.firstCardSuit = firstCard.suit
+            self.firstCardValue = firstCard.value
+            self.secondCardSuit = secondCard.suit
+            self.secondCardValue = secondCard.value
+            
+            // fetch card images
+            
+            
+        }
     }
     
     func compareCards() {
+        
+        
         // pass in values of card 1 and card 2
         // compare card values
         // if player guess matches card values, score +1 and call startRound()
@@ -42,7 +66,7 @@ class GamePlayController {
     }
     
     func incrementStickFigureImage() {
-        // increment stick figure image index
+        // increment stick figure image index; once reaches 4, end the game
         if stickFigureImageIndex <= 2 {
             stickFigureImageIndex += 1
         } else {
@@ -52,6 +76,8 @@ class GamePlayController {
     
     func endGame() {
         endGameFlag = true
+        // display some sort of message that the game has ended
+        // option to start a new game?
     }
 }
 
